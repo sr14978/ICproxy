77c85d621dd8caf2ea6a1b60d9f5f5ca7d7d1fe7
sr14978 <sr14978@my.bristol.ac.uk>
2018-04-22 19:55:26 +0100

    import

--- fteproxy/record_layer.py
+++ fteproxy/record_layer.py
@@ -7,7 +7,7 @@ import fte.encoder
 
 import fteproxy.conf
 from fte.encrypter import Encrypter
-import innocuous_ciphertexts.emulator.conf
+import innocuous_ciphertexts.emulator as emulator
 
 MAX_CELL_SIZE = fteproxy.conf.getValue('runtime.fteproxy.record_layer.max_cell_size')
 
