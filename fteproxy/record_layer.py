2419a833d5e60441e3b62d585f614af535cad548
sr14978 <sr14978@my.bristol.ac.uk>
2018-04-23 00:28:37 +0100

    3 stage system

--- fteproxy/record_layer.py
+++ fteproxy/record_layer.py
@@ -16,15 +16,21 @@ class Encoder:
 
     def __init__(
         self,
+        encrypter,
+        to_length_dist,
         encoder,
     ):
+        self._encrypter = encrypter
+        self._to_length_dist = to_length_dist
         self._encoder = encoder
-        self._buffer = ''
+        self._clear_buffer = ''
+        self._encrypted_buffer = []
+        self._length_dist_buffer = []
 
     def push(self, data):
         """Push data onto the FIFO buffer."""
 
-        self._buffer += data
+        self._clear_buffer += data
 
     def pop(self):
         """Pop data off the FIFO buffer. We pop at most
@@ -32,13 +38,18 @@ class Encoder:
         bytes. The returned value is encrypted and encoded
         with ``encoder`` specified in ``__init__``.
         """
-        retval = ''
 
-        while len(self._buffer) > 0:
-            plaintext = self._buffer[:emulator.conf.frag_plaintext_length]
-            covertext = self._encoder.encode(plaintext)
-            self._buffer = self._buffer[emulator.conf.frag_plaintext_length:]
-            retval += covertext
+        while len(self._clear_buffer) > 0:
+            plaintext = self._clear_buffer[:emulator.conf.frag_plaintext_length]
+            ciphertext = self._encrypter.encrypt(plaintext)
+            self._clear_buffer = self._clear_buffer[emulator.conf.frag_plaintext_length:]
+            _encrypted_buffer.append(ciphertext)
+
+        self._length_dist_buffer = self._to_length_dist(self._encrypted_buffer)
+        self._encrypted_buffer = []
+
+        retval = "".join([self._encoder.encode(m) for m in self._length_dist_buffer])
+        self._length_dist_buffer = []
 
         return retval
 
@@ -47,15 +58,21 @@ class Decoder:
 
     def __init__(
         self,
+        decrypter,
+        from_length_dist,
         decoder,
     ):
+        self._decrypter = decrypter
+        self._from_length_dist = from_length_dist
         self._decoder = decoder
-        self._buffer = ''
+        self._proxy_buffer = ''
+        self._length_dist_buffer = []
+        self._encrypted_buffer = []
 
     def push(self, data):
         """Push data onto the FIFO buffer."""
 
-        self._buffer += data
+        self._proxy_buffer += data
 
     def pop(self, oneCell=False):
         """Pop data off the FIFO buffer.
@@ -63,27 +80,21 @@ class Decoder:
         with ``_decrypter`` specified in ``__init__``.
         """
 
-        retval = ''
-
-        while len(self._buffer) > 0:
+        while len(self._proxy_buffer) > 0:
             try:
-                msg, buffer = self._decoder.decode(self._buffer)
-                retval += msg
-                self._buffer = buffer
-            except fte.encoder.DecodeFailureError as e:
-                fteproxy.info("fteproxy.encoder.DecodeFailure: "+str(e))
-                break
-            except fte.encrypter.RecoverableDecryptionError as e:
-                fteproxy.info("fteproxy.encrypter.RecoverableDecryptionError: "+str(e))
-                break
-            except fte.encrypter.UnrecoverableDecryptionError as e:
-                fteproxy.fatal_error("fteproxy.encrypter.UnrecoverableDecryptionError: "+str(e))
-                # exit
+                msg, buffer = self._decoder.decode(self._proxy_buffer)
+                self._length_dist_buffer.append(msg)
+                self._proxy_buffer = buffer
             except Exception as e:
                 fteproxy.warn("fteproxy.record_layer exception: "+str(e))
                 break
             finally:
                 if oneCell:
                     break
+
+        self._encrypted_buffer, self._length_dist_buffer = self._from_length_dist(self._length_dist_buffer)
+
+        retval = "".join([self._decrypter.decrypt(c) for c in self._encrypted_buffer])
+        self._encrypted_buffer = []
 
         return retval
